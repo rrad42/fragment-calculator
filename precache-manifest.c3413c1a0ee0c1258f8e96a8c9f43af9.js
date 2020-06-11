9a1c3e069800e1a497e1dd5b52cccfcf58026035
rrad <rrad@localhost>
2020-06-11 15:02:49 -0700

    Updates

---
+++ precache-manifest.c3413c1a0ee0c1258f8e96a8c9f43af9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3af33b023c242e4e9d3fa7838a37095",
+    "url": "/fragment-calculator/index.html"
+  },
+  {
+    "revision": "67361d0b26c5826c3338",
+    "url": "/fragment-calculator/static/js/2.49d1451b.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/fragment-calculator/static/js/2.49d1451b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2b73a782f0ae941dc8",
+    "url": "/fragment-calculator/static/js/main.f1ac67d6.chunk.js"
+  },
+  {
+    "revision": "a54cb9145e3c39541138",
+    "url": "/fragment-calculator/static/js/runtime-main.55338fbf.js"
+  }
+]);
